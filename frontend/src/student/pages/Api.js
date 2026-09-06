c44589dea83ddd4b3be725bbe7ded741bb15f386
karan1828p <156888690+karan1828p@users.noreply.github.com>
2026-09-06 11:10:03 +0530

    done

--- frontend/src/student/pages/Api.js
+++ frontend/src/student/pages/Api.js
@@ -2,6 +2,6 @@
 export const BACKEND_URL = 
   import.meta.env.MODE === 'development'
     ? 'http://localhost:4000/api' // Change 5000 if your local backend runs on a different port (e.g., 8000)
-    : 'https://hackathon-qyq7.onrender.com';
+    : 'https://hackathon-qyq7.onrender.com/api';
 
  
